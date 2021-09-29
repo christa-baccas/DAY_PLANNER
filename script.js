1096ceb90d283ddca7b79dfc4dddbaac111b7e3d
christa-baccas <christa.baccas@gmail.com>
2021-09-28 21:39:50 -0500

    fixed error in the hml timebox container

--- script.js
+++ script.js
@@ -13,6 +13,31 @@ currentDate();
 // if(currentTime ===){
 
 // }
+function currentTime() {
+    var time = moment().hours()
+    console.log(current);
+    var hour = $('.hour');
+
+    //    console.log(current);
+
+    // time.each(function () {
+    //   var hour = parseInt($(this).attr('id'))
+
+    // //  console.log(hour);
+
+    //   if (hour === current) {
+    //     $(this).children('.col-sm-10').attr('class', 'present col-sm-10 description')
+    // //    console.log(('present' + hour + current))
+    //   } else if (current > hour) {
+    //     $(this).children('.col-sm-10').attr('class', 'past col-sm-10 description')
+    // //    console.log(('past' + hour + current))
+    //   } else {
+    //     $(this).children('.col-sm-10').attr('class', 'future col-sm-10 description')
+    // //    console.log(('future' + hour + current))
+    //   }
+    // })
+  }
+  currentTime()
 
 
 // when I click into a timeblock I can enter an event
